91aac64f1c14c2d162671e18e2cd3377e95a35b8
Mark Njenga <marknjenga25@gmail.com>
2024-10-21 09:07:21 +0300

    app.py py updated

--- server/app.py
+++ server/app.py
@@ -1,55 +1,58 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Plant
+from flask import Flask, jsonify, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from models import Plant, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
 
-api = Api(app)
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant_by_id(id):
+    plant = Plant.query.get(id)
+    if plant:
+        return jsonify({
+            'id': plant.id,
+            'name': plant.name,
+            'image': plant.image,
+            'price': plant.price,
+            'is_in_stock': plant.is_in_stock
+        }), 200
+    else:
+        abort(404, description="Plant not found")
 
-
-class Plants(Resource):
-
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        abort(404, description="Plant not found")
+    
+    data = request.get_json()
+    if "is_in_stock" in data:
+        plant.is_in_stock = data["is_in_stock"]
         db.session.commit()
+        return jsonify({
+            'id': plant.id,
+            'name': plant.name,
+            'image': plant.image,
+            'price': plant.price,
+            'is_in_stock': plant.is_in_stock
+        }), 200
 
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+    return jsonify({"error": "Invalid input"}), 400
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
-
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        abort(404, description="Plant not found")
+    
+    db.session.delete(plant)
+    db.session.commit()
+    
+    return '', 204
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
